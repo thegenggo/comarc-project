41ed10276c06213a66c7adeb9901bbea119278a1
Kisane <kritsanaphong-keang@hotmail.com>
2022-10-13 11:14:29 +0700

    add read instructions from file

--- assembler/main.cpp
+++ assembler/main.cpp
@@ -1,45 +1,117 @@
-#include <iostream>
-#include <fstream>
-#include <string>
+/* Assembler code fragment */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 #define MAXLINELENGTH 1000
 
-using namespace std;
-
-int readAndParse(string, string &, string &, string &, string &, string &);
+int readAndParse(FILE *, char *, char *, char *, char *, char *);
+int isNumber(char *);
 
 int main(int argc, char *argv[])
 {
-    string myText;
-    ofstream outputFile("output.txt");
-    ifstream inputFile("input.txt");
-    while (getline(inputFile, myText))
-    {
-        cout << myText << endl;
-        string label, opcode, arg0, arg1, arg2;
-        readAndParse(myText, label, opcode, arg0, arg1, arg2);
+    char *inFileString, *outFileString;
+    FILE *inFilePtr, *outFilePtr;
+    char label[MAXLINELENGTH], opcode[MAXLINELENGTH], arg0[MAXLINELENGTH],
+            arg1[MAXLINELENGTH], arg2[MAXLINELENGTH];
+
+    if (argc != 3) {
+        printf("error: usage: %s <assembly-code-file> <machine-code-file>\n",
+            argv[0]);
+        exit(1);
     }
-    return 0;
+
+    inFileString = argv[1];
+    outFileString = argv[2];
+
+    inFilePtr = fopen(inFileString, "r");
+    if (inFilePtr == NULL) {
+        printf("error in opening %s\n", inFileString);
+        exit(1);
+    }
+    outFilePtr = fopen(outFileString, "w");
+    if (outFilePtr == NULL) {
+        printf("error in opening %s\n", outFileString);
+        exit(1);
+    }
+
+    /* here is an example for how to use readAndParse to read a line from
+        inFilePtr */
+    if (! readAndParse(inFilePtr, label, opcode, arg0, arg1, arg2) ) {
+        /* reached end of file */
+    }
+
+    /* read instructions from file */
+    while(readAndParse(inFilePtr, label, opcode, arg0, arg1, arg2)) {
+        printf("%s %s %s %s %s\n", label, opcode, arg0, arg1,arg2);
+    }
+
+    /* this is how to rewind the file ptr so that you start reading from the
+        beginning of the file */
+    rewind(inFilePtr);
+
+    /* after doing a readAndParse, you may want to do the following to test the
+        opcode */
+    if (!strcmp(opcode, "add")) {
+        /* do whatever you need to do for opcode "add" */
+    }
+
+    return(0);
 }
 
-int readAndParse(string instruction, string &label, string &opcode, string &arg0,
-                 string &arg1, string &arg2)
+/*
+ * Read and parse a line of the assembly-language file.  Fields are returned
+ * in label, opcode, arg0, arg1, arg2 (these strings must have memory already
+ * allocated to them).
+ *
+ * Return values:
+ *     0 if reached end of file
+ *     1 if all went well
+ *
+ * exit(1) if line is too long.
+ */
+int readAndParse(FILE *inFilePtr, char *label, char *opcode, char *arg0,
+    char *arg1, char *arg2)
 {
-    char labelChar[MAXLINELENGTH], opcodeChar[MAXLINELENGTH], arg0Char[MAXLINELENGTH],
-        arg1Char[MAXLINELENGTH], arg2Char[MAXLINELENGTH];
+    char line[MAXLINELENGTH];
+    char *ptr = line;
 
-    if (sscanf(instruction.c_str(), "%[^\t\n ]", labelChar))
-    {
-        label = labelChar;
+    /* delete prior values */
+    label[0] = opcode[0] = arg0[0] = arg1[0] = arg2[0] = '\0';
+
+    /* read the line from the assembly-language file */
+    if (fgets(line, MAXLINELENGTH, inFilePtr) == NULL) {
+	/* reached end of file */
+        return(0);
     }
 
-    sscanf(instruction.c_str() + label.length(), "%*[\t\n ]%[^\t\n ]%*[\t\n ]%[^\t\n ]%*[\t\n ]%[^\t\n ]%*[\t\n ]%[^\t\n ]",
-        opcodeChar, arg0Char, arg1Char, arg2Char);
+    /* check for line too long (by looking for a \n) */
+    if (strchr(line, '\n') == NULL) {
+        /* line too long */
+	printf("error: line too long\n");
+	exit(1);
+    }
 
-    opcode = opcodeChar;
-    arg0 = arg0Char;
-    arg1 = arg1Char;
-    arg2 = arg2Char;
-    
-    return 0;
+    /* is there a label? */
+    ptr = line;
+    if (sscanf(ptr, "%[^\t\n ]", label)) {
+	/* successfully read label; advance pointer over the label */
+        ptr += strlen(label);
+    }
+
+    /*
+     * Parse the rest of the line.  Would be nice to have real regular
+     * expressions, but scanf will suffice.
+     */
+    sscanf(ptr, "%*[\t\n ]%[^\t\n ]%*[\t\n ]%[^\t\n ]%*[\t\n ]%[^\t\n ]%*[\t\n ]%[^\t\n ]",
+        opcode, arg0, arg1, arg2);
+    return(1);
+}
+
+int isNumber(char *string)
+{
+    /* return 1 if string is a number */
+    int i;
+    return( (sscanf(string, "%d", &i)) == 1);
 }
