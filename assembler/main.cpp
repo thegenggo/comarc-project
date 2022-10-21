dce4bff789a82f0b4baba3a33e2b412a65bbaffb
thegenggo <kritsanaphong-keang@hotmail.com>
2022-10-21 17:48:58 +0700

    clean code

--- assembler/main.cpp
+++ assembler/main.cpp
@@ -244,11 +244,7 @@ int convertOpcodeToInterger(const char *opcode)
         return 0b000;
     }
 
-<<<<<<< HEAD
     printf("error: opcode is not define.");
-=======
-    printf("error: can not define opcode");
->>>>>>> 0da4018066ee3db92972d75d05412209a5c82818
     exit(1);
 }
 
